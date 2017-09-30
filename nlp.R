195f292ff0c9ddb65054065bb04057a6e8b28570
Jeff Blackadar <jeffblackadar@gmail.com>
2017-09-29 22:41:25 -0400

    Adding code to save a processed status on a source document

--- nlp.R
+++ nlp.R
@@ -7,19 +7,26 @@ library(openNLP)
 library(RWeka)
 #install.packages("RMySQL")
 library(RMySQL)
-options(java.parameters = "- Xmx1024m")
+options(java.parameters = "-Xmx4096m")
 
 
-printAndStoreEntities<-function(entityInEdition,outputFileHtmlCon, entity_table){
+
+printAndStoreEntities<-function(entityInEdition,outputFileHtmlCon, entity_table, entity_table_id_name, entity_document_x_table, entity_document_x_table_id_name, table_id_source_document){
   topicsInEditionString<-paste(entityInEdition, collapse=', ' )
   thisCell<-paste('<td>',topicsInEditionString,'</td>',sep="")
   writeLines(thisCell,outputFileHtmlCon)
   
   for(entity in entityInEdition) {
     entitySql = gsub("'", "&apos;", entity)
+    entitySql = gsub("’", "&apos;", entity)
     entitySql = gsub("\n", "", entitySql)
     entitySql = gsub("\'", "", entitySql)
     entitySql = gsub("\\", "", entitySql, fixed=TRUE)
+    
+    
+    if(nchar(entitySql)>99){
+      entitySql=substr(entitySql,1,99)
+    }
     #Store people into database
     #first, check if it is already in the database
     query <-
@@ -36,18 +43,51 @@ printAndStoreEntities<-function(entityInEdition,outputFileHtmlCon, entity_table)
     if (nrow(dbRows) == 0) {
       query <-
         paste(
-          "INSERT INTO ",entity_table," (name) VALUES('",
+          "INSERT INTO ",entity_table," (name) VALUES(LEFT(RTRIM('",
           entitySql,
-          "')",
+          "'),99))",
           sep = ''
         )
       print (query)
       rsInsert = dbSendQuery(mydb, query)
-      dbClearResult(rsInsert)
+      #dbClearResult(rsInsert)
     }
-
     
-    dbClearResult(rs)
+    
+    #dbClearResult(rs)
+    
+    #do cross reference
+    
+    # get handle on ID    
+    query<-paste("select ",entity_table_id_name," from ",entity_table," where name='",entitySql,"'",sep='')
+    rs = dbSendQuery(mydb,query)
+    dbRows<-dbFetch(rs)
+    if (nrow(dbRows)==0){
+      print (paste("Problem: zero rows for ",query,sep=''))
+    } 
+    
+    else {
+      print (dbRows[[1]])
+      entity_table_id<-dbRows[[1]]
+      
+      query<-paste("select ",entity_document_x_table_id_name," from ",entity_document_x_table," where ", entity_document_x_table_id_name, "=",entity_table_id," and id_source_document=", table_id_source_document,sep='')
+      rs = dbSendQuery(mydb,query)
+      dbRows<-dbFetch(rs)
+      if (nrow(dbRows)==0){
+        
+        
+        query <-
+          paste(
+            "INSERT INTO ",entity_document_x_table," (", entity_document_x_table_id_name,", id_source_document) VALUES(",
+            entity_table_id,",",table_id_source_document,
+            ")",
+            sep = ''
+          )
+        print (query)
+        rsInsert = dbSendQuery(mydb, query)
+        #dbClearResult(rsInsert)
+      }
+    }
   }
 }
 
@@ -65,7 +105,7 @@ mydb = dbConnect(MySQL(), user='localuser', password=localuserpassword, dbname='
 #mydb2 <- dbConnect(RMySQL::MySQL(), group = "corpus_entities")
 
 
-dbListTables(mydb)
+#dbListTables(mydb)
 #dbListFields(mydb, 'sourcedocument')
 
 
@@ -83,9 +123,10 @@ entities <- function(doc, kind) {
 
 #openconnecton to read file
 #inputFile <- "equityurls.txt"
+inputFile <- "equityurls_1883-1999.txt"
 #using a shorter test file, each edition takes a long time.
 #inputFile <- "equityurls_1970-08.txt"
-inputFile <- "equityurls_test2.txt"
+#inputFile <- "equityurls_test2.txt"
 
 inputCon  <- file(inputFile, open = "r")
 
@@ -181,10 +222,10 @@ while (length(urlLine <-
       dbClearResult(rsInsert)
     }
     
-    dbClearResult(rs)
+    #dbClearResult(rs)
     
     # get handle on ID    
-    query<-paste("select id_source_document from source_documents where source_document_name='",editionFileName,"'",sep='')
+    query<-paste("select id_source_document,source_document_processed from source_documents where source_document_name='",editionFileName,"'",sep='')
     rs = dbSendQuery(mydb,query)
     dbRows<-dbFetch(rs)
     if (nrow(dbRows)==0){
@@ -194,101 +235,65 @@ while (length(urlLine <-
     else {
       print (dbRows[[1]])
       id_source_document<-dbRows[[1]]
-      #Perform Natural Language Processing on the file
-      
-      inputEquityTextFile <-paste("c:\\a_orgs\\carleton\\hist3814\\equity\\",editionFileName,".txt",sep="")
-      
-      inputEquityTextFileCon  <- file(inputEquityTextFile, open = "r")
-      equityEditionText <-
-        paste(readLines(inputEquityTextFileCon, n = -1, warn = TRUE),
-              collapse = "\n")
-      close(inputEquityTextFileCon)
-      
-      equityEditionString <- as.String(equityEditionText)
-      
-      word_ann <- Maxent_Word_Token_Annotator()
-      sent_ann <- Maxent_Sent_Token_Annotator()
-      
-      equityEdition_annotations <-
-        annotate(equityEditionString, list(sent_ann, word_ann))
-      # class(equityEdition_annotations)
-      # head(equityEdition_annotations)
-      equityEdition_doc <-
-        AnnotatedPlainTextDocument(equityEditionString, equityEdition_annotations)
-      sents(equityEdition_doc)
-      
-      # This does not work for me.
-      #sents(bio_doc) %>% head(2)
-      
-      # bio = equityEditionString
-      # bio_annotations = equityEdition_annotations
-      # bio_doc = equityEdition_doc
-      
-      
-      person_ann <- Maxent_Entity_Annotator(kind = "person")
-      location_ann <- Maxent_Entity_Annotator(kind = "location")
-      organization_ann <- Maxent_Entity_Annotator(kind = "organization")
-      
-      pipeline <- list(sent_ann,
-                       word_ann,
-                       person_ann,
-                       location_ann,
-                       organization_ann)
-      equityEdition_annotations <- annotate(equityEditionString, pipeline)
-      equityEdition_doc <-
-        AnnotatedPlainTextDocument(equityEditionString, equityEdition_annotations)
-      
-      peopleInEdition<-c(sort(unique(entities(equityEdition_doc, kind = "person"))))
-      
-      printAndStoreEntities(peopleInEdition,outputFilePeopleHtmlCon, "entities_people")
-      
-      # topicsInEditionString<-paste(peopleInEdition, collapse=', ' )
-      # thisCell<-paste('<td>',topicsInEditionString,'</td>',sep="")
-      # writeLines(thisCell,outputFilePeopleHtmlCon)
-      # 
-      # for(person in peopleInEdition){
-      #   
-      #   #Store people into database
-      #   #first, check if it is already in the database
-      #   query<-paste("select * from entities_people where name='",gsub("'", "&apos;", person),"'",sep='')
-      #   print (query)
-      #   rs = dbSendQuery(mydb,query)
-      #   dbRows<-dbFetch(rs)
-      #   #dbFetch() always returns a data.frame with as many rows as records were fetched and as many columns as fields in the result set, even if the result is a single value or has one or zero rows
-      #   
-      #   if (nrow(dbRows)==0){
-      #     query<-paste("INSERT INTO entities_people (name) VALUES('",gsub("'", "&apos;", person),"')",sep='')
-      #     print (query)
-      #     rsInsert = dbSendQuery(mydb,query)
-      #     dbClearResult(rsInsert)
-      #   }
-      #   
-      #   dbClearResult(rs)
-      # }
-      
-      
-      
-      locationsInEdition<-sort(unique(entities(equityEdition_doc, kind = "location")))
-      printAndStoreEntities(locationsInEdition,outputFileLocationsHtmlCon, "entities_locations")
-      
-      # topicsInEditionString<-paste(c(locationsInEdition), collapse=', ' )
-      # thisCell<-paste('<td>',topicsInEditionString,'</td>',sep="")
-      # writeLines(thisCell,outputFileLocationsHtmlCon)
-      
-      
-      organizationsInEdition<-sort(unique(entities(equityEdition_doc, kind = "organization")))
-      printAndStoreEntities(organizationsInEdition,outputFileOrganizationsHtmlCon, "entities_organizations")
-      
-      # topicsInEditionString<-paste(c(organizationsInEdition), collapse=', ' )
-      # thisCell<-paste('<td>',topicsInEditionString,'</td>',sep="")
-      # writeLines(thisCell,outputFileOrganizationsHtmlCon)
-      
-      
-      
-      
-      writeLines('</tr>', outputFilePeopleHtmlCon)
-      writeLines('</tr>', outputFileLocationsHtmlCon)
-      writeLines('</tr>', outputFileOrganizationsHtmlCon)
+      source_document_processed<-dbRows[[2]]
+      #Was this row already processed? If it was source_document_processed=1.
+      if (source_document_processed==0){
+        #Perform Natural Language Processing on the file
+        
+        inputEquityTextFile <-paste("c:\\a_orgs\\carleton\\hist3814\\equity\\",editionFileName,".txt",sep="")
+        
+        inputEquityTextFileCon  <- file(inputEquityTextFile, open = "r")
+        equityEditionText <-
+          paste(readLines(inputEquityTextFileCon, n = -1, warn = TRUE),
+                collapse = "\n")
+        close(inputEquityTextFileCon)
+        
+        equityEditionString <- as.String(equityEditionText)
+        
+        word_ann <- Maxent_Word_Token_Annotator()
+        sent_ann <- Maxent_Sent_Token_Annotator()
+        
+        equityEdition_annotations <-
+          annotate(equityEditionString, list(sent_ann, word_ann))
+        # class(equityEdition_annotations)
+        # head(equityEdition_annotations)
+        equityEdition_doc <-
+          AnnotatedPlainTextDocument(equityEditionString, equityEdition_annotations)
+        sents(equityEdition_doc)
+        
+        
+        person_ann <- Maxent_Entity_Annotator(kind = "person")
+        location_ann <- Maxent_Entity_Annotator(kind = "location")
+        organization_ann <- Maxent_Entity_Annotator(kind = "organization")
+        
+        pipeline <- list(sent_ann,
+                         word_ann,
+                         person_ann,
+                         location_ann,
+                         organization_ann)
+        equityEdition_annotations <- annotate(equityEditionString, pipeline)
+        equityEdition_doc <-
+          AnnotatedPlainTextDocument(equityEditionString, equityEdition_annotations)
+        
+        peopleInEdition<-c(sort(unique(entities(equityEdition_doc, kind = "person"))))
+        printAndStoreEntities(peopleInEdition,outputFilePeopleHtmlCon, "entities_people", "id_entities_person", "people_x_sourcedocuments", "id_entities_person",id_source_document)
+        
+        locationsInEdition<-sort(unique(entities(equityEdition_doc, kind = "location")))
+        printAndStoreEntities(locationsInEdition,outputFileLocationsHtmlCon, "entities_locations", "id_entities_location", "locations_x_sourcedocuments", "id_entities_location",id_source_document)
+        
+        organizationsInEdition<-sort(unique(entities(equityEdition_doc, kind = "organization")))
+        printAndStoreEntities(organizationsInEdition,outputFileOrganizationsHtmlCon, "entities_organizations", "id_entities_organization", "organizations_x_sourcedocuments", "id_entities_organization",id_source_document)
+        
+        
+        writeLines('</tr>', outputFilePeopleHtmlCon)
+        writeLines('</tr>', outputFileLocationsHtmlCon)
+        writeLines('</tr>', outputFileOrganizationsHtmlCon)
+        
+        #Now that the processing is complete, update the row in source_documents to indicate that.
+        query<-paste("update source_documents SET source_document_processed=1 where id_source_document =",id_source_document,"",sep='')
+        rs = dbSendQuery(mydb,query)
+        print(query)
+      }
     }
   }
 }
@@ -303,6 +308,7 @@ writeLines('</table></body></html>', outputFileOrganizationsHtmlCon)
 close(outputFilePeopleHtmlCon)
 close(outputFileLocationsHtmlCon)
 close(outputFileOrganizationsHtmlCon)
+
 
 
 
