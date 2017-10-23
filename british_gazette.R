8192fc2fcca034f538af5de5f35954afc8569cd6
Jeff Blackadar <jeffblackadar@gmail.com>
2017-10-22 21:35:27 -0400

    clean up

--- british_gazette.R
+++ british_gazette.R
@@ -1,12 +1,10 @@
-
-
-
-outputFileCsv <- "gazette_potato_wart.csv"
+outputFileCsv <- "gazette_potato_warts.csv"
 outputFileCsvCon<-file(outputFileCsv, open = "w")
 
-
 library(XML)
-xml_file <- "https://www.thegazette.co.uk/all-notices/notice/data.feed?end-publish-date=1918-11-11&text=potatoes+wart+schedule&start-publish-date=1914-08-03&location-distance-1=1&service=all-notices&categorycode-all=all&numberOfLocationSearches=1&results-page=20"
+#xml_file <- "https://www.thegazette.co.uk/all-notices/notice/data.feed?end-publish-date=1918-11-11&text=potatoes+wart+schedule&start-publish-date=1914-08-03&location-distance-1=1&service=all-notices&categorycode-all=all&numberOfLocationSearches=1&results-page=20"
+#Don't think I need the results page
+xml_file <- "https://www.thegazette.co.uk/all-notices/notice/data.feed?end-publish-date=1918-11-11&text=potatoes+wart+schedule&start-publish-date=1914-08-03&location-distance-1=1&service=all-notices&categorycode-all=all&numberOfLocationSearches=1"
 xmlfile <- xmlTreeParse(readLines(xml_file)[1])
 
 topxml <- xmlRoot(xmlfile)
@@ -15,9 +13,10 @@ xml_df <- data.frame(t(topxml), row.names=NULL)
 totalPagesReturned<-as.integer(xml_df$total)
 
 if(totalPagesReturned>0){
-  #for(gatherPagesCounter in 0:(floor(totalPagesReturned/10))+1){
+  #read in 10 pages at a time
+  for(gatherPagesCounter in 0:(floor(totalPagesReturned/10))+1){
   # for now, so that I donw stress out the server I am getting only the first pages
-  for(gatherPagesCounter in 0:0+1){
+  #for(gatherPagesCounter in 0:0+1){
     print (gatherPagesCounter)
     
     xml_file <- paste("https://www.thegazette.co.uk/all-notices/notice/data.feed?end-publish-date=1918-11-11&text=potatoes+wart+schedule&start-publish-date=1914-08-03&location-distance-1=1&service=all-notices&categorycode-all=all&numberOfLocationSearches=1&results-page=",(gatherPagesCounter),sep='')
@@ -99,6 +98,7 @@ if(totalPagesReturned>0){
         }
         
         writeLines(paste("\"",entryId,"\",\"",entryTitle,"\",\"",entryUpdated,"\",\"",entryPublished,"\",\"",noticeText,"\"",sep=""),outputFileCsvCon)
+        #writeLines(paste("\"",entryTitle,"\",\"",entryUpdated,"\",\"",entryPublished,"\",\"",noticeText,"\"",sep=""),outputFileCsvCon)
         
         
       }
@@ -109,8 +109,6 @@ if(totalPagesReturned>0){
       p1 <- proc.time()
       Sys.sleep(2)
       proc.time() - p1
-      
-      
     }
   }
   
@@ -119,55 +117,3 @@ if(totalPagesReturned>0){
 }
 
 close(outputFileCsvCon)
-
-#xml_df
-# 
-# xml_df$page.number[1]
-# xml_df$page.size[1]
-# xml_df$page.start[1]
-# xml_df$page.stop[1]
-# xml_df$total[1]
-# 
-# 
-# 
-# entry<-xml_df$entry.2[1]
-# entry[[1]]$id
-# entry[[1]]$title
-# entry[[1]]$updated
-# entry[[1]]$published
-
-
-
-#Thanks to: https://dzone.com/articles/reading-and-text-mining-pdf
-
-# here is a pdf for mining
-
-# url <- "https://www.thegazette.co.uk/London/issue/28981/page/9556/data.pdf"
-# dest <- tempfile(fileext = ".pdf")
-# download.file(url, dest, mode = "wb")
-# 
-# # set path to pdftotxt.exe and convert pdf to text
-# 
-# exe <- "C:\\a_orgs\\carleton\\hist3814\\R\\graham_fellowship\\pdftools\\bin64\\pdftotext.exe"
-# system(paste("\"", exe, "\" \"", dest, "\"", sep = ""), wait = F)
-# 
-# # get txt-file name and open it
-# filetxt <- sub(".pdf", ".txt", dest)
-# shell.exec(filetxt); shell.exec(filetxt) # strangely the first try always throws an error..
-# 
-# 
-# library(tm)
-# 
-# txt <- readLines(filetxt) # don't mind warning..
-
-
-# 
-# 
-# xml_file <- "https://www.thegazette.co.uk/notice/29815/data.xml"
-# xmlfile <- xmlTreeParse(readLines(xml_file)[1])
-# #class(xmlfile)
-# topxml <- xmlRoot(xmlfile)
-# topxml <- xmlSApply(topxml,function(x) xmlSApply(x, xmlValue))
-# xml_df <- data.frame(t(topxml), row.names=NULL)
-# xml_df$total
-
