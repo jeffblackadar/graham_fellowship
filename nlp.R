56f2cd833712c6263f596f70cb179dbe56a8e451
Jeff Blackadar <jeffblackadar@gmail.com>
2017-12-10 09:35:17 -0500

    Added spell correction

--- nlp.R
+++ nlp.R
@@ -5,58 +5,179 @@ library(rJava)
 library(NLP)
 library(openNLP)
 library(RWeka)
-#install.packages("RMySQL")
 library(RMySQL)
+library(qdap)
+library(stringr)
+
+
+
+#for spell checking
 options(java.parameters = "-Xms4096m -Xmx4096m")
+
+#set value of tab character
+tabChar = 9
+mode(tabChar) <- "raw"
+
+spellCheckOrig<-c("")
+spellCheckCorrect<-c("")
+spellCheckNorvig<-c("")
+
+
+# Read in big.txt, a 6.5 mb collection of different English texts.
+raw_text <- paste(readLines("C:/a_orgs/carleton/hist3814/R/graham_fellowship/norvigs-big-plus-placesx2.txt"), collapse = " ")
+# Make the text lowercase and split it up creating a huge vector of word tokens.
+split_text <- strsplit(tolower(raw_text), "[^a-z]+")
+# Count the number of different type of words.
+word_count <- table(split_text)
+# Sort the words and create an ordered vector with the most common type of words first.
+sorted_words <- names(sort(word_count, decreasing = TRUE))
 
 setwd("C:/a_orgs/carleton/hist3814/R/graham_fellowship")
 
+#Rasmus Bååth's Research Blog
+#http://www.sumsar.net/blog/2014/12/peter-norvigs-spell-checker-in-two-lines-of-r/
+correctNorvig <- function(word) {
+  # Calculate the edit distance between the word and all other words in sorted_words.
+  edit_dist <- adist(word, sorted_words)
+  # Calculate the minimum edit distance to find a word that exists in big.txt 
+  # with a limit of two edits.
+  min_edit_dist <- min(edit_dist, 2)
+  # Generate a vector with all words with this minimum edit distance.
+  # Since sorted_words is ordered from most common to least common, the resulting
+  # vector will have the most common / probable match first.
+  proposals_by_prob <- c(sorted_words[ edit_dist <= min(edit_dist, 2)])
+  # In case proposals_by_prob would be empty we append the word to be corrected...
+  proposals_by_prob <- c(proposals_by_prob, word)
+  # ... and return the first / most probable word in the vector.
+  proposals_by_prob[1]
+}
+
+
+
 printAndStoreEntities<-function(entityInEdition,outputFileHtmlCon, entity_table, entity_table_id_name, entity_document_x_table, entity_document_x_table_id_name, table_id_source_document){
+  spellCheckOrig<-c("")
+  spellCheckMisSpelled<-c("")
+  spellCheckCorrect<-c("")
+  spellCheckNorvig<-c("")
+  
   topicsInEditionString<-paste(entityInEdition, collapse=', ' )
   thisCell<-paste('<td>',topicsInEditionString,'</td>',sep="")
 #  writeLines(thisCell,outputFileHtmlCon)
   
   for(entity in entityInEdition) {
-    entitySql = gsub("'", "''", entity)
-    entitySql = gsub("’", "''", entity)
-    entitySql = gsub(char(9), " ", entity)
-    entitySql = gsub("\n", "", entitySql)
-    entitySql = gsub("\'", "''", entitySql)
-    entitySql = gsub("\\", "", entitySql, fixed=TRUE)
     
     
-    if(nchar(entitySql)>99){
-      entitySql=substr(entitySql,1,99)
+    #entitySql = gsub("'", "''", entity)
+    #entitySql = gsub("’", "''", entitySql)
+    #entitySql = gsub(tabChar, " ", entitySql)
+    #entitySql = gsub("\n", " ", entitySql)
+    #entitySql = gsub("\t", " ", entitySql)
+    #entitySql = gsub("\'", "''", entitySql)
+    #entitySql = gsub("\\", "", entitySql, fixed=TRUE)
+    
+    correctedEntity = entity
+    correctedEntity = gsub(tabChar, " ", correctedEntity)
+    correctedEntity = gsub("\n", " ", correctedEntity)
+    correctedEntity = gsub("\t", " ", correctedEntity)
+    correctedEntitySql = gsub("'", "''", correctedEntity)
+    correctedEntitySql = gsub("’", "''", correctedEntitySql)
+    correctedEntitySql = gsub("\'", "''", correctedEntitySql)
+    correctedEntitySql = gsub("\\", "", correctedEntitySql, fixed=TRUE)
+    
+    
+    if(nchar(correctedEntitySql)>99){
+      correctedEntitySql=substr(correctedEntitySql,1,99)
     }
     #Store people into database
     #first, check if it is already in the database
     query <-
       paste("select * from ",entity_table," where name='",
-            entitySql,
+            correctedEntitySql,
             "'",
             sep = '')
-    print (query)
+    #print (query)
     
     rs = dbSendQuery(mydb, query)
     dbRows <- dbFetch(rs)
     #dbFetch() always returns a data.frame with as many rows as records were fetched and as many columns as fields in the result set, even if the result is a single value or has one or zero rows
     
     if (nrow(dbRows) == 0) {
+      
+      #------- Do spell check only on new words 
+      nameSpellChecked=""
+      
+      correctedEntityWords = strsplit(correctedEntity, " ")
+      correctedEntityWordsNorvig = strsplit(correctedEntity, " ")
+      
+      #spelledWords = which_misspelled(correctedEntityWords, suggest=FALSE)
+      misSpelledWords <-tryCatch(
+        {
+          which_misspelled(correctedEntity, suggest=TRUE)
+        },
+        error=function(cond) {
+          NULL
+        },
+        warning=function(cond) {
+          NULL
+        },
+        finally={
+          NULL
+        })
+      
+      
+      if(is.null(misSpelledWords)){
+        
+        misSpelled=FALSE
+      } else {
+        for(counter in 1:length(misSpelledWords[[1]])){
+          misSpelled=TRUE
+          wordNum = as.integer(misSpelledWords[[1]][counter])
+          correctedEntityWords[[1]][wordNum] = misSpelledWords[counter,3]
+          correctedEntityWordsNorvig[[1]][wordNum] = correctNorvig(correctedEntityWordsNorvig[[1]][wordNum])
+        }
+        correctedEntitySpellChecked = paste(correctedEntityWords[[1]],collapse=" ")
+        correctedEntityNorvig = paste(correctedEntityWordsNorvig[[1]],collapse=" ")
+        nameSpellChecked=""
+        if(!str_to_upper(correctedEntity)==str_to_upper(correctedEntityNorvig)){
+          #We have found a suggested correction
+          nameSpellChecked=correctedEntityNorvig
+          
+          print(paste(correctedEntity,misSpelled,correctedEntitySpellChecked,correctedEntityNorvig,sep="  ---  "))
+          
+          #keep a vector of the words to make into a dataframe so that we can check the results of the spell check.  Remove this after training of the spell checker is done.
+          #spellCheckOrig<-c(spellCheckOrig,correctedEntity)
+          #spellCheckMisSpelled<-c(spellCheckMisSpelled,misSpelled)
+          #spellCheckCorrect<-c(spellCheckCorrect,correctedEntitySpellChecked)
+          #spellCheckNorvig<-c(spellCheckNorvig,correctedEntityNorvig) 
+          
+        }
+      }
+
+      #Clean up any symbols that will cause an SQL error when inserted into the database
+      nameSpellCheckedSql = gsub("'", "''", nameSpellChecked)
+      nameSpellCheckedSql = gsub("’", "''", nameSpellCheckedSql)
+      nameSpellCheckedSql = gsub("\'", "''", nameSpellCheckedSql)
+      nameSpellCheckedSql = gsub("\\", "", nameSpellCheckedSql, fixed=TRUE)
+      
+      #-------
+
       query <-
         paste(
-          "INSERT INTO ",entity_table," (name) VALUES(LEFT(RTRIM('",
-          entitySql,
+          "INSERT INTO ",entity_table," (name, name_spell_checked) VALUES(LEFT(RTRIM('",
+          correctedEntitySql,
+          "'),99),LEFT(RTRIM('",
+          nameSpellCheckedSql,
           "'),99))",
           sep = ''
         )
-      print (query)
+      #print (query)
       rsInsert = dbSendQuery(mydb, query)
     }
     
     #do cross reference
     
     # get handle on ID    
-    query<-paste("select ",entity_table_id_name," from ",entity_table," where name='",entitySql,"'",sep='')
+    query<-paste("select ",entity_table_id_name," from ",entity_table," where name='",correctedEntitySql,"'",sep='')
     rs = dbSendQuery(mydb,query)
     dbRows<-dbFetch(rs)
     if (nrow(dbRows)==0){
@@ -64,14 +185,14 @@ printAndStoreEntities<-function(entityInEdition,outputFileHtmlCon, entity_table,
     } 
     
     else {
-      print (dbRows[[1]])
+      #print (dbRows[[1]])
       entity_table_id<-dbRows[[1]]
       
       query<-paste("select ",entity_document_x_table_id_name," from ",entity_document_x_table," where ", entity_document_x_table_id_name, "=",entity_table_id," and id_source_document=", table_id_source_document,sep='')
       rs = dbSendQuery(mydb,query)
       dbRows<-dbFetch(rs)
       if (nrow(dbRows)==0){
-
+        
         query <-
           paste(
             "INSERT INTO ",entity_document_x_table," (", entity_document_x_table_id_name,", id_source_document) VALUES(",
@@ -79,18 +200,21 @@ printAndStoreEntities<-function(entityInEdition,outputFileHtmlCon, entity_table,
             ")",
             sep = ''
           )
-        print (query)
+        
+        #print (query)
         rsInsert = dbSendQuery(mydb, query)
       }
     }
   }
+  #spellCheckDF<-data.frame(spellCheckOrig,spellCheckNorvig)
+  #edit(spellCheckDF)
 }
 
 library(RMySQL)
 rmysql.settingsfile<-"C:\\ProgramData\\MySQL\\MySQL Server 5.7\\corpus_entities_with_misc.cnf"
 
 rmysql.db<-"corpus_entities_with_misc"
-storiesDb<-dbConnect(RMySQL::MySQL(),default.file=rmysql.settingsfile,group=rmysql.db)
+mydb<-dbConnect(RMySQL::MySQL(),default.file=rmysql.settingsfile,group=rmysql.db)
 
 
 
@@ -171,7 +295,7 @@ while (length(urlLine <-
 
     if (nrow(dbRows)==0){
       query<-paste("INSERT INTO source_documents (source_document_name,source_document_base_url,source_document_file_extension_1,source_document_file_extension_2) VALUES('",editionFileName,"','",urlLine,"','pdf','txt')",sep='')
-      print (query)
+      #print (query)
       rsInsert = dbSendQuery(mydb,query)
       dbClearResult(rsInsert)
     }
@@ -216,14 +340,12 @@ while (length(urlLine <-
         person_ann <- Maxent_Entity_Annotator(kind = "person")
         location_ann <- Maxent_Entity_Annotator(kind = "location")
         organization_ann <- Maxent_Entity_Annotator(kind = "organization")
-        misc_ann <- Maxent_Entity_Annotator(kind = "misc")
-        
+
         pipeline <- list(sent_ann,
                          word_ann,
                          person_ann,
                          location_ann,
-                         organization_ann,
-                         misc_ann)
+                         organization_ann)
         equityEdition_annotations <- annotate(equityEditionString, pipeline)
         equityEdition_doc <-
           AnnotatedPlainTextDocument(equityEditionString, equityEdition_annotations)
@@ -237,18 +359,18 @@ while (length(urlLine <-
         organizationsInEdition<-sort(unique(entities(equityEdition_doc, kind = "organization")))
         printAndStoreEntities(organizationsInEdition,outputFileOrganizationsHtmlCon, "entities_organizations", "id_entities_organization", "organizations_x_sourcedocuments", "id_entities_organization",id_source_document)
         
-        miscInEdition<-sort(unique(entities(equityEdition_doc, kind = "misc")))
-        printAndStoreEntities(miscInEdition,outputFileMiscHtmlCon, "entities_misc", "id_entities_misc", "misc_x_sourcedocuments", "id_entities_misc",id_source_document)
-
         #Now that the processing is complete, update the row in source_documents to indicate that.
         query<-paste("update source_documents SET source_document_processed=1 where id_source_document =",id_source_document,"",sep='')
         rs = dbSendQuery(mydb,query)
-        print(query)
+        #print(query)
         
       }
     }
   }
-  objectsToKeep<-c("inputCon", "mydb","urlLine","entities","printAndStoreEntities" )
+  
+  
+
+  objectsToKeep<-c("correctNorvig","tabChar", "inputCon", "mydb","urlLine","entities","printAndStoreEntities","word_count", "sorted_words")
   rm(list=setdiff(ls(),objectsToKeep ))
   gc()
 }
@@ -258,4 +380,6 @@ close(inputCon)
 dbDisconnect(mydb)
 
 gc()
+
+
 
